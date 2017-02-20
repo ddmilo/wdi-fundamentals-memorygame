d542ed0316c67ad7b4a32c455b766f15fb9ba735
Dan Milo <ddmilo1029@gmail.com>
2017-02-19 19:24:11 -0500

    created game logic

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,6 +1,6 @@
 console.log("JS file is connected to HTML! Woo!")
 
-/* var cardOne = "Queen";
+var cardOne = "Queen";
 var cardTwo = "Queen";
 var cardThree = "King";
-var cardFour = "King"; */
+var cardFour = "King";
